b9bacd3c889734aebf52106e6f0877bdbe91f5b2
martijor000 <82613327+martijor000@users.noreply.github.com>
2024-05-12 20:12:00 -0700

    Connected home page to profile page

--- app/src/main/java/com/example/captone_coffee_talk/HomePage.kt
+++ app/src/main/java/com/example/captone_coffee_talk/HomePage.kt
@@ -1,11 +1,19 @@
 package com.example.captone_coffee_talk
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class HomePage : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page) // Assuming home_page.xml is your layout file
+        val profileBtn = findViewById<ImageButton>(R.id.profileButton)
+        profileBtn.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }
     } // Other methods and logic specific to your HomePage class
 }
