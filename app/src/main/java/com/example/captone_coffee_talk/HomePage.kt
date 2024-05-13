689e854152df9f9b6766ceee1f14c4c1e82077c4
martijor000 <82613327+martijor000@users.noreply.github.com>
2024-05-12 19:31:31 -0700

    Added home page ui once user is logged in

---
+++ app/src/main/java/com/example/captone_coffee_talk/HomePage.kt
@@ -0,0 +1,11 @@
+package com.example.captone_coffee_talk
+
+import android.app.Activity
+import android.os.Bundle
+
+class HomePage : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_page) // Assuming home_page.xml is your layout file
+    } // Other methods and logic specific to your HomePage class
+}
