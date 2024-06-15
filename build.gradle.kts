0730dc8ddd1b3fc212e4809f13efac3402d23c10
TrevorE177 <TrevorE177@gmail.com>
2024-06-15 12:07:20 -0700

    Please Work :(

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,28 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all subprojects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+
+//        maven {url = uri("https://maven.testfairy.com")}
+//        maven {url = uri("https://example.com/maven")}
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+    task ("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+    allprojects {
+        repositories {
+            google()
+        }
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-
+    id("com.android.library") version "8.2.0" apply false
 }
